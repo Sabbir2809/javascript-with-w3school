269c2ccbcbe1b15d56e137415cdbab1c27fdc7f2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-09 21:39:27 +0600

    JS Output

---
+++ Tutorial_1 Output/main.js
@@ -0,0 +1,5 @@
+// To access an HTML element, JavaScript can use the document.getElementById(id) method
+document.getElementById("innerHTML").innerHTML = "11";
+
+// For debugging purposes, you can call the console.log() method in the browser to display data.
+console.log(5 + 6);
