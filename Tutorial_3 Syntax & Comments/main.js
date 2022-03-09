0ff10d15b710c4acf732556a1591ccc16db8fad6
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-09 23:34:54 +0600

    JS Syntax

---
+++ Tutorial_3 Syntax & Comments/main.js
@@ -0,0 +1,20 @@
+// How to create variables:
+var x;
+let y;
+
+// How to use variables:
+x = 5;
+y = 6;
+// JavaScript uses arithmetic operators ( + - * / ) to compute values:
+let z = x + y;
+
+// JavaScript is Case Sensitive
+let lastname, lastName;
+lastName = "Khan";
+lastname = "Hossain";
+
+// JavaScript programmers tend to use camelCase that starts with a lowercase letter:
+let firstName, masterCard, interCity;
+
+/* JavaScript comments can be used to explain JavaScript code, and to make it more readable. JavaScript comments can also be used to prevent execution, when testing alternative code. */
+
