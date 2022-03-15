0cd20a23030d40958cc04cf73dc5cd31946517b6
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-15 23:00:59 +0600

    Operators

--- Tutorial_6 Operators/operators.js
+++ Tutorial_6 Operators/operators.js
@@ -1,17 +1,34 @@
 // Operators
 // Arithmetic Operators(+, -, *, **, /, %, ++, --)
-let x = 5;
+let x = 10;
 let y = 2;
+x--;
+y++;
+// precedence: * > / > + > -
 let add = x + y;
-let sub = x * y;
+let subtraction = x - y;
+let multiplication = x * y;
+let exponentiation = x ** y; // Math.pow(x,y)
+let division = x / y;
+let modulus = x % y;
 
-console.log(add);
-console.log(sub);
 
 // Assignment Operators (=, +=, -=, *=, /=, %=)
 let a = 10;
-a += 5;
-console.log(a);
+a += 5; // a = a + 5;
+
+let b = 10;
+b -= 5; // b = b - 5;
+
+let c = 10;
+c *= 5; // c = c * 5;
+
+let d = 10;
+d /= 5; // d = d / 5;
+
+let e = 10;
+e %= 5; // e = e % 5;
+
 
 // String Operators
 let text1 = "Sabbir";
