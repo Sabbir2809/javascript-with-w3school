85fd2f3f0072bc0139cfa485103490de110b6213
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-15 22:40:28 +0600

    JS Operators

---
+++ Tutorial_6 Operators/operators.js
@@ -0,0 +1,38 @@
+// Operators
+// Arithmetic Operators(+, -, *, **, /, %, ++, --)
+let x = 5;
+let y = 2;
+let add = x + y;
+let sub = x * y;
+
+console.log(add);
+console.log(sub);
+
+// Assignment Operators (=, +=, -=, *=, /=, %=)
+let a = 10;
+a += 5;
+console.log(a);
+
+// String Operators
+let text1 = "Sabbir";
+let text2 = "Hossain";
+let text = text1 + " " + text2;
+console.log(text);
+
+// Adding Strings and Numbers
+let x1 = 5 + 5;
+let x2 = "5" + 5;
+let x3 = "Hello" + 5;
+console.log(x1);
+console.log(x2);
+console.log(x3);
+
+// Comparison Operators (==, ===, !=, !==, >, <, >=, <=, ?)
+
+// Logical Operators (&&, ||, !)
+
+// Type Operators (typeof(), instanceof())
+
+
+
+
