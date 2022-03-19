c82139978cc7948b6f9a515ffa2f160629bb28b2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-19 09:39:39 +0600

    JS Strings

--- Tutorial_11 String/string.js
+++ Tutorial_11 String/string.js
@@ -5,3 +5,23 @@ let varsity = "DIU"; // Single quotes
 // String Length
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 console.log(alphabet.length);
+
+// Escape Character (\', \", \\, \b, \f, \n, \r, \t, \v)
+let text1 = 'We are\n the\f so-called\n "Vikings" from the north';
+let text2 = "It's alright. Hello \
+Dolly!";
+let text3 = "The character \\ is called ackslash.";
+console.log(text1);
+
+// JavaScript Strings as Objects
+let x = "Sabbir";
+let y = new String("Sabbir"); // new Keyword Complicates the code and slows down execution speed.
+console.log(typeof x);
+console.log(typeof y);
+
+let ansX = (x == y);
+let ansY = (x === y);
+console.log(ansX);
+console.log(ansY);
+
+
