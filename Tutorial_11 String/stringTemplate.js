e05d31c3f7914dc80ff8d39e37fd7033c0e7a4a8
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-03-26 19:11:00 +0600

    JS Strings

---
+++ Tutorial_11 String/stringTemplate.js
@@ -0,0 +1,21 @@
+// JavaScript Template Literals
+
+// Template Literals use back-ticks (``)
+let text1 = `I'm a Web Developer.`;
+console.log(text1);
+
+// Quotes Inside Strings
+let text2 = `"HTML", "CSS", "JavaScript"`;
+console.log(text2);
+
+// Multiline Strings
+let bio = `I love to explore new technologies. 
+I enjoy programming and working on various projects, which I sometimes share on Github.`;
+console.log(bio);
+
+// Variable Substitutions
+let versity = "DIU";
+let department = "CSE";
+
+let education = `Now, I'm Studying at ${versity} in the Department of ${department}.`
+console.log(education);
